c42616ba7b8e45ad0f9fd4f82dd93079656556d2
Mahadevan Kallat <kallatmahadevan@gmail.com>
2026-02-17 11:41:21 +0530

    feature(UC3): refactor banner using functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,40 @@
 public class OOPSBannerApp {
 
+    static String[] O = {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+    };
+
+    static String[] P = {
+            " ***** ",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      "
+    };
+
+    static String[] S = {
+            " ***** ",
+            "*      ",
+            " ***** ",
+            "      *",
+            " ***** "
+    };
+
+    static void printBanner() {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        printBanner();
 
     }
 }
