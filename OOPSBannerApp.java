a391b08d8b174420cce678bd6a5056f2479c22c1
Mahadevan Kallat <kallatmahadevan@gmail.com>
2026-02-17 11:42:50 +0530

    feature(UC4,UC5): implement CharacterPattern class and Map-based banner rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,40 +1,63 @@
+import java.util.*;
+
+class CharacterPattern {
+
+    String[] pattern;
+
+    CharacterPattern(String[] pattern) {
+        this.pattern = pattern;
+    }
+}
+
 public class OOPSBannerApp {
 
-    static String[] O = {
-            " ***** ",
-            "*     *",
-            "*     *",
-            "*     *",
-            " ***** "
-    };
+    static Map<Character, CharacterPattern> patterns = new HashMap<>();
 
-    static String[] P = {
-            " ***** ",
-            "*     *",
-            " ***** ",
-            "*      ",
-            "*      "
-    };
+    static {
 
-    static String[] S = {
-            " ***** ",
-            "*      ",
-            " ***** ",
-            "      *",
-            " ***** "
-    };
+        patterns.put('O', new CharacterPattern(new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        }));
 
-    static void printBanner() {
+        patterns.put('P', new CharacterPattern(new String[] {
+                " ***** ",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      "
+        }));
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+        patterns.put('S', new CharacterPattern(new String[] {
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        }));
+
+    }
+
+    static void printBanner(String text) {
+
+        for (int row = 0; row < 5; row++) {
+
+            for (char c : text.toCharArray()) {
+
+                System.out.print(patterns.get(c).pattern[row] + " ");
+            }
+
+            System.out.println();
         }
 
     }
 
     public static void main(String[] args) {
 
-        printBanner();
+        printBanner("OOPS");
 
     }
 }
