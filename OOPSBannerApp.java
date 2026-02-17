545a36d897488811ef0ca61c3084dd101539eb9e
Mahadevan Kallat <kallatmahadevan@gmail.com>
2026-02-17 15:14:28 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,45 @@
 /**
  * OOPSBannerApp
  *
- * Goal: UC2 - Display "OOPS" in Banner Format using '*' and spaces.
+ * Goal: UC3 - Display "OOPS" in Banner Format using String.join()
+ *
+ * Refactored to avoid inefficient string concatenation.
  *
  * @author Mahadevan
- * @version 2.0
+ * @version 3.0
  */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        String line1 = String.join(" ",
+                "*****",
+                "*****",
+                "*****",
+                "*****");
 
+        String line2 = String.join(" ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *");
+
+        String line3 = String.join(" ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *");
+
+        String line4 = String.join(" ",
+                "*****",
+                "*****",
+                "*****",
+                "*****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
     }
 }
